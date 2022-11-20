fbc0b9b758aa32a723bdb7cf235278e67bcdc143
GustavoDSCruz <gustavodiasdsc@gmail.com>
2022-11-20 17:08:09 -0300

    pizza menu and buy's screens with mysql

---
+++ php/pedido.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'conectar.php';
+
+class pedido{
+
+    private $id;
+    private $nome;
+    private $telefone;
+    private $cep;
+    private $complemento;
+    private $id_pizza;
+    private $conn;
+
+    
+
+}
+?>
